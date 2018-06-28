a35bbf95a840eb4d6caf34a641faf5015d719925
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-28 10:14:19 +0100

    add note list view test and functionality

--- spec/note-list-model-test.js
+++ spec/note-list-model-test.js
@@ -1,9 +1,8 @@
 (function(exports){
   function testStoreArrayOfNoteModels(){
-    var note = new Note("text");
     var noteList = new NoteList();
-    noteList.addNote(note);
-    assert.isIncluded(note, noteList.viewNotes())
+    noteList.addNote('note');
+    assert.isIncluded('note', noteList.viewNotes())
   };
   testStoreArrayOfNoteModels();
 })(this);
